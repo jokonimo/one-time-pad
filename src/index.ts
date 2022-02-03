c79b2d35b7b0e429b9042348c601ed957f01e329
jokonimo <jonas.kolbeck@gmx.de>
2022-02-03 15:40:02 +0100

    :sparkles: adds encryption with addition

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
-export const encryptAndDecrypt = (message: string, key: string): string => {
-    //check if message and key are same length
-    if (message.length !== key.length) {
-        throw new Error("key needs to be same length");
-    }
+export const encryptDecryptXOR = (message: string, key: string): string => {
+    checkParams(message, key);
     let encryptedString = "";
-    //xor each message key with each key
     for (let index = 0; index < message.length; index++) {
         encryptedString += String.fromCharCode(message.charCodeAt(index) ^ key.charCodeAt(index));
     }
-    //success
     return encryptedString;
+}
+
+export const encryptAddition = (message: string, key: string): string => {
+    checkParams(message, key);
+    let encryptedString = "";
+    for (let index = 0; index < message.length; index++) {
+        encryptedString += message.charCodeAt(index) + key.charCodeAt(index)
+    }
+    return encryptedString;
+}
+
+export const decryptAdditon = (message: string, key: string): string => {
+    checkParams(message, key);
+    let decryptedString = "";
+    for (let index = 0; index < message.length; index++) {
+        decryptedString += message.charCodeAt(index) + key.charCodeAt(index)
+    }
+    return decryptedString;
+}
+
+const checkParams = (message: string, key: string) => {
+    if (message.length < key.length) {
+        throw new Error("key needs to atleast length of the message");
+    }
 }
